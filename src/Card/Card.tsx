2e79ebdadc77655eee37c9484ceba61284eac5d5
olga <goncherenkoola@gmail.com>
2022-08-14 18:06:27 +0300

    card with list

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,32 +1,35 @@
 import React from "react";
 import "./Card.css";
 
-interface Props {
+export type CardType = {
+  id: number;
   name: string;
   description: string;
   category: string;
   price: number;
   rating: number;
   imageUrl: string;
+};
+
+interface Props {
+  data: CardType;
 }
 
-// function Card(props: Props) {
 class Card extends React.Component<Props> {
   render() {
+    // object destructuring
+    const { data } = this.props;
+
     return (
-      <div className="card">
-        <img
-          src={this.props.imageUrl}
-          alt={this.props.name}
-          className="card-img-top"
-        />
-        <div>{this.props.category}</div>
+      <div className="card border-0 m-4 shadow bg-dark bg-opacity-10">
+        <img src={data.imageUrl} alt={data.name} className="card-img-top" />
+        <div>{data.category}</div>
         <div className="card-body">
-          <div className="card-title">{this.props.name}</div>
-          <div className="card-text">{this.props.description}</div>
-          <div>{this.props.price}</div>
+          <div className="card-title">{data.name}</div>
+          <div className="card-text">{data.description}</div>
+          <div>{data.price}</div>
           <div>
-            Rating: {this.props.rating}
+            Rating: {data.rating}
             <i className="bi-star"></i>
           </div>
           <button className="btn btn-primary">Order Now</button>
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Card from "./Card/Card";
+import Card, { CardType } from "./Card/Card";
+import "./Menu.css";
 
 type displayMode = "grid" | "list";
 
@@ -9,6 +10,7 @@ interface MenuProps {
 
 interface MenuState {
   display: displayMode;
+  cards: Array<CardType>;
 }
 
 class Menu extends React.Component<MenuProps, MenuState> {
@@ -17,24 +19,64 @@ class Menu extends React.Component<MenuProps, MenuState> {
 
     this.state = {
       display: props.defaultDisplay,
+      cards: [
+        {
+          id: 1,
+          name: "Fried Chicken",
+          description: "fried chicken with chips",
+          category: "Chicken",
+          price: 15.99,
+          rating: 5,
+          imageUrl:
+            "https://cdn.pixabay.com/photo/2016/04/25/07/32/chicken-cutlet-1351331__340.jpg",
+        },
+        {
+          id: 2,
+          name: "Greek Salad",
+          description: "salad with olives",
+          category: "vegeterian",
+          price: 10.55,
+          rating: 5,
+          imageUrl:
+            "https://cdn.pixabay.com/photo/2016/08/09/10/30/tomatoes-1580273__340.jpg",
+        },
+        {
+          id: 3,
+          name: "Pad Tai",
+          description: "very good",
+          category: "asian",
+          price: 22.85,
+          rating: 5,
+          imageUrl:
+            "https://cdn.pixabay.com/photo/2015/04/10/00/41/food-715542__340.jpg",
+        },
+      ],
     };
   }
 
   changeDisplay = (mode: displayMode) => {
+    // 1) I was missing parenthesis
     // this.setState((state, props) => {
     //     display: mode
     // })
 
-    this.setState({
-      ...this.state,
+    // 2) this.props and this.state may be updated asynchronously - get use to this
+    this.setState((state, props) => ({
       display: mode,
-    });
+    }));
+
+    // works in this case - no call to this.props and this.state
+    // this.setState({
+    //     ...this.state,
+    //     display: mode
+    // })
   };
 
   render() {
     return (
       <>
-        <h1>Order delievery or takeaway</h1>
+        <h1>Order Delivery or Takeaway</h1>
+
         <div className="d-flex">
           <button
             onClick={(e) => this.changeDisplay("list")}
@@ -50,37 +92,10 @@ class Menu extends React.Component<MenuProps, MenuState> {
           </button>
         </div>
 
-        <div
-          className={
-            this.state.display === "list" ? "d-flex flex-column" : "d-flex"
-          }
-        >
-          <Card
-            name="Fried Chicken"
-            description="fried chicken with chips"
-            category="Chicken"
-            price={15.99}
-            rating={5}
-            imageUrl="https://cdn.pixabay.com/photo/2016/04/25/07/32/chicken-cutlet-1351331__340.jpg"
-          />
-
-          <Card
-            name="Greek Salad"
-            description="salad with olives"
-            category="vegeterian"
-            price={10.55}
-            rating={5}
-            imageUrl="https://cdn.pixabay.com/photo/2016/08/09/10/30/tomatoes-1580273__340.jpg"
-          />
-
-          <Card
-            name="Pad Tai"
-            description="very good"
-            category="asian"
-            price={22.85}
-            rating={5}
-            imageUrl="https://cdn.pixabay.com/photo/2015/04/10/00/41/food-715542__340.jpg"
-          />
+        <div className={this.state.display}>
+          {this.state.cards.map((card) => (
+            <Card key={card.id} data={card} />
+          ))}
         </div>
       </>
     );
