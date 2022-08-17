ff1255b90a55947c534665170e9fbd6e198c2c1c
olga <goncherenkoola@gmail.com>
2022-08-17 19:35:30 +0300

    adding cards

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var path = require("path");
+const headers = require("./middleware/headers");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var cardsRouter = require("./routes/cards");
-const headers = require("./middleware/headers");
+
 var app = express();
 
 app.use(logger("dev"));
@@ -14,9 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(headers);
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("./cards", cardsRouter);
+app.use("/cards", cardsRouter);
 
 module.exports = app;
